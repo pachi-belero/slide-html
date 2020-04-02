95af05c3486c7371b76bcb82d23232545a3d264e
Joel (archface) <jloow89@gmail.com>
2020-04-02 19:32:38 +0200

    add seperate file for customization

--- build.js
+++ build.js
@@ -5,10 +5,12 @@ var cssmin = require('cssmin');
 
 var css = fs.readFileSync("src/slide.css", encoding='utf8');
 var js = fs.readFileSync("src/slide.js", encoding='utf8');
+var customization = fs.readFileSync("src/customization.css", encoding='utf8');
 var html = fs.readFileSync("src/slide.html", encoding='utf8');
 
 var inline = html
   .replace(/<script src="slide.js"><\/script>/, '<script>'+uglify(js, {fromString: true}).code+'</script>')
   .replace(/<link .*href="slide.css">/, '<style>'+cssmin(css)+'</style>');
 
-console.log(inline);
+fs.writeFileSync("slide.html", html);
+fs.writeFileSync("customization.css", customization);
